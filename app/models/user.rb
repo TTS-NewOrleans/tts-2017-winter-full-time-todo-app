8220a728e97a0202889fe7b98f302986af59abb3
Ryan Hageman <improv42@gmail.com>
2017-03-07 09:42:13 -0600

    Setup OmniAuth table and model
    * Setup parsing of Facebook API hash
    * Create methods for getting user avatar

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+
+  # Create a user with the hash from the Facebook API
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      user.name = auth['info']['name']
+    end
+  end
+
+  # Pulls the avatar from FB
+  def small_image
+    "http://graph.facebook.com/#{self.uid}/picture?type=small"
+  end
+
+  def normal_image
+    "http://graph.facebook.com/#{self.uid}/picture?type=normal"
+  end
+end
